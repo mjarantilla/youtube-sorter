22c7e093e8cb978875868213957a0b0bcfa0622a
Michael Jarantilla <michaelj@dogvacay.com>
2020-04-26 17:22:13 -0700

    added main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from handlers import client, playlist, ranks, utilities
+import argparse
 
+def main():
+    args = {}
+    flags = {}
+    parser = argparse.ArgumentParser(description='Process some integers.')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
