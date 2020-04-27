ca08f94adcf84d30c1d78425dbed0dbf3c9958b7
Michael Jarantilla <michaelj@dogvacay.com>
2020-04-26 17:13:04 -0700

    added RanksHandler class

--- handlers/ranks.py
+++ handlers/ranks.py
@@ -1,26 +1,33 @@
-from handlers import client
+from handlers import client, playlist
 import json
 from handlers import utilities
+
 
 class Tier:
     def __init__(self, **kwargs):
         self.name = kwargs['tier']
         self.subtiers = None
-        self.get_subtiers()
         self.channels = kwargs['channels'] if 'channels' in kwargs else []
-        self.channel_upload_playlists = []
-        self.get_channel_ids()
+        self.channel_data = []
         self.separate = kwargs['separate'] if 'separate' in kwargs and kwargs['separate'] else False
-        self.playlist_id = kwargs['playlist_id'] if 'playlist_id' in kwargs else None
+        self.playlist = kwargs['playlist'] if 'playlist' in kwargs else None
         self.videos = []
+        if 'subtiers' in kwargs:
+            self.get_subtiers(**kwargs)
+        self.get_channel_data()
 
     def get_subtiers(self, **kwargs):
         self.subtiers = []
         subtiers = kwargs['subtiers']
         for subtier_kwargs in subtiers:
-            if 'playlist_id' not in subtier_kwargs and self.playlist_id is not None:
-                subtier_kwargs['playlist_id'] = self.playlist_id
+            if 'separate' not in subtier_kwargs:
+                subtier_kwargs['separate'] = self.separate
+            if 'playlist' not in subtier_kwargs and self.playlist is not None:
+                subtier_kwargs['playlist'] = self.playlist
             self.subtiers.append(Tier(**subtier_kwargs))
+
+        if len(self.subtiers) == 0:
+            self.subtiers = None
 
     def assemble_video_list(self):
         full_list = self.videos
@@ -29,20 +36,38 @@ class Tier:
             subtier.assemble_video_list()
             full_list = full_list + subtier.videos
 
-    def get_channel_ids(self):
-        self.channel_upload_playlists = []
+    def get_channel_data(self):
+        self.channel_data = []
         config = utilities.ConfigHandler()
-        subscriptions = json.load(open(config.subscriptions_filepath, mode='r'))
+        subscriptions = json.load(open(config.subscriptions_filepath, mode='r'))['details']
 
         for channel_name in self.channels:
-            self.channel_upload_playlists.append(subscriptions[channel_name]['uploads'])
+            self.channel_data.append(subscriptions[channel_name])
 
     def get_videos(self):
         self.videos = []
-        youtube = client.YoutubeClientHandler()
 
     def print_rank(self):
         return None
+
+
+class RanksHandler():
+    def __init__(self):
+        config = utilities.ConfigHandler()
+        self.data = json.load(open(config.ranks_filepath, mode='r'))
+        self.ranks = self.data['ranks']
+        self.filtered = self.data['filters']
+        self.playlists = self.data['playlist_ids']
+        for tier in config.variables['TIER_PLAYLISTS']:
+            self.playlists[tier] = config.variables['TIER_PLAYLISTS'][tier]
+        self.rank_data = []
+
+    def define_ranks(self):
+        for rank_block in self.ranks:
+            if 'playlist' not in rank_block:
+                rank_block['playlist'] = 'watch_later'
+            rank = Tier(**rank_block)
+            self.rank_data.append(rank)
 
 class Autolister:
     def __init__(self):
