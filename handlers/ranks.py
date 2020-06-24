38d65a01a07670c8a1d13d379e6bba21035ee4f4
mjaranti <michael.e.jarantilla@jpl.nasa.gov>
2020-06-24 12:50:17 -0700

    initial version of sorter algorithms

--- handlers/ranks.py
+++ handlers/ranks.py
@@ -1,29 +1,42 @@
 from handlers import client, queue
 import json
+import threading
 from handlers import utilities
 
 
 class Tier:
     def __init__(self, **kwargs):
+        self.kwargs = kwargs
         self.name = kwargs['tier']
         self.subtiers = []
         self.channels = kwargs['channels'] if 'channels' in kwargs else []
         self.channel_data = []
         self.separate = kwargs['separate'] if 'separate' in kwargs and kwargs['separate'] else False
-        self.playlist = kwargs['playlist'] if 'playlist' in kwargs else None
+        self.playlist_id = kwargs['playlist_id'] if 'playlist_id' in kwargs else None
+        if self.playlist_id == "":
+            self.playlist_id = None
+        self.backlog_id = kwargs['backlog_id'] if 'backlog_id' in kwargs else None
+        if self.backlog_id == "":
+            self.backlog_id = None
         self.videos = []
         if 'subtiers' in kwargs:
             self.get_subtiers(**kwargs)
         self.get_channel_data()
 
     def get_subtiers(self, **kwargs):
+        if len(kwargs) == 0:
+            kwargs = self.kwargs
         self.subtiers = []
         subtiers = kwargs['subtiers']
+        count = 0
         for subtier_kwargs in subtiers:
+            if 'tier' not in subtier_kwargs:
+                count += 1
+                subtier_kwargs['tier'] = "%s_%s" % (self.name, str(count).zfill(2))
             if 'separate' not in subtier_kwargs:
                 subtier_kwargs['separate'] = self.separate
-            if 'playlist' not in subtier_kwargs and self.playlist is not None:
-                subtier_kwargs['playlist'] = self.playlist
+            if 'playlist' not in subtier_kwargs and self.playlist_id is not None:
+                subtier_kwargs['playlist'] = self.playlist_id
             self.subtiers.append(Tier(**subtier_kwargs))
 
     def assemble_video_list(self):
@@ -59,6 +72,7 @@ class RanksHandler():
         self.config = utilities.ConfigHandler()
         self.data = json.load(open(self.config.ranks_filepath, mode='r'))
         self.ranks = self.data['ranks']
+        self.tiers = []
         self.filtered = self.data['filters']
         self.filtered_channels = []
         for channel_name in self.filtered['channels']:
@@ -83,33 +97,60 @@ class RanksHandler():
         else:
             return False
 
-    def get_ordered_channels(self, tier):
-        def get_tier_channels(data):
-            channels = []
+    def get_playlists_and_channels(self, tier):
+        def get_tier_channels(data, channels, parent_playlist=None):
             if 'channels' in data:
-                channels = data['channels']
+                if 'playlist_id' in data or parent_playlist is None:
+                    if data['playlist_id'] not in channels:
+                        channels[data['playlist_id']] = []
+                    channels[data['playlist_id']] += data['channels']
+                else:
+                    if parent_playlist not in channels:
+                        channels[parent_playlist] = []
+                    channels[parent_playlist] += data['channels']
+
             if 'subtiers' in data:
-                for subtier in data['subtiers']:
-                    channels = channels + get_tier_channels(subtier)
+                counter = 0
+                for subtier_data in data['subtiers']:
+                    if 'tier' not in subtier_data:
+                        counter += 1
+                        subtier_data['tier'] = "%s_%s" % (data['tier'], str(counter).zfill(2)  )
+                    channels = get_tier_channels(subtier_data, channels, parent_playlist)
             return channels
 
+        tier_data = None
         for tier_data in self.ranks:
             if tier_data["tier"] == tier:
                 break
+            tier_data = None
 
-        if tier_data["tier"] == tier:
-            results = get_tier_channels(tier_data)
+        if tier_data is not None:
+            if tier_data["tier"] == tier:
+                channels = {}
+                if 'playlist_id' in tier_data:
+                    playlist = tier_data['playlist_id']
+                else:
+                    playlist = "PL8wvcc8NSIHKg8C1O39efqI4KSNwivMAw"
+                results = get_tier_channels(
+                    data=tier_data,
+                    channels=channels,
+                    parent_playlist=playlist
+                )
 
-            subscriptions = json.load(open(self.config.subscriptions_filepath, mode='r'))['details']
-            id_list = []
-            for result in results:
-                id_list.append(subscriptions[result]['id'])
-
-            return id_list
+                subscriptions = json.load(open(self.config.subscriptions_filepath, mode='r'))['details']
+                # id_list = []
+                # for result in results:
+                #     # id_list.append(subscriptions[result]['id'])
+                #     id_list.append(result)  # Indexing by channel title rather than channel ID
+                #
+                # return id_list
+                return results
         return []
 
-    def get_tier_playlist_id(self, tier):
-        return None
+
+    def get_tiers(self):
+        for tier_data in self.ranks:
+            self.tiers.append(Tier(**tier_data))
 
 
 class Autolister:
--- handlers/sorter.py
+++ handlers/sorter.py
@@ -1,12 +1,30 @@
 from handlers import client, utilities, ranks
+import threading
 
 
-class YoutubePlaylist:
+class YoutubePlaylist(threading.Thread):
     def __init__(self, **kwargs):
-        self.tier = kwargs['tier']
-        self.id = kwargs['id']
-        self.videos = []
+        # Expected kwargs:
+        # - name
+        # - id (optional)
+
+        super().__init__()
+        self.ranks = ranks.RanksHandler()
         self.client = client.YoutubeClientHandler().get_client()
+        self.name = kwargs['name']
+
+        # Sets playlist ID to what is submitted as a kwarg.
+        # If no ID is set in kwarg, look up playlist ID by name in ranks.json.
+        # If no ID is present in ranks.json, throw an error.
+        self.id = kwargs['id'] if 'id' in kwargs else None
+        if self.id is None and self.name in self.ranks.playlists:
+            self.id = self.ranks.playlists[self.name]
+        if self.id is None:
+            raise ValueError("Invalid playlist; no playlist ID supplied")
+
+        # Resources to use internally
+        self.ordered_channels = []
+        self.videos = []
         self.new_items_queue = []
         self.deleted_items_queue = []
         self.backlog = []
@@ -15,7 +33,7 @@ class YoutubePlaylist:
         kwargs = {
             'playlistId': self.id,
             'maxResults': 50,
-            'part': "snippet,contentDetails,id"
+            'part': "contentDetails"
         }
 
         request = self.client.playlistItems().list(**kwargs)
@@ -28,14 +46,142 @@ class YoutubePlaylist:
             response = request.execute()
             items = items + response['items']
 
+        vid_ids = []
+        for item in items:
+            vid_ids.append(item['contentDetails']['videoId'])
+
+        id_list = ",".join(vid_ids)
+        kwargs = {
+            "part": "snippet",
+            "id": id_list,
+            "maxResults": 50
+        }
+        request = self.client.videos().list(**kwargs)
+        response = request.execute()
+        items = response['items']
+
+        while 'nextPageToken' in response:
+            kwargs['pageToken'] = response['nextPageToken']
+            request = self.client.videos().list(**kwargs)
+            response = request.execute()
+            items = items + response['items']
+
         self.videos = items
 
+    def get_ordered_channels(self):
+        def add_subtier_channels(playlist_name, tier_data):
+            subtier_channels = []
+            tier = tier_data['tier']
+            if 'playlist_id' in tier_data:
+                if tier_data['playlist_id'] == playlist_name:
+                    if 'channels' in tier_data:
+                        for channel_name in tier_data['channels']:
+                            subtier_channels.append(channel_name)
+            if 'subtiers' in tier_data:
+                counter = 0
+                for subtier_data in tier_data['subtiers']:
+                    if 'tier' not in subtier_data:
+                        subtier_data['tier'] = "%s_%s" % (tier, str(counter).zfill(2))
+                    if 'playlist_id' in tier_data and 'playlist_id' not in subtier_data:
+                        subtier_data['playlist_id'] = tier_data['playlist_id']
+                    subtier_channels += add_subtier_channels(playlist_name, subtier_data)
+
+            self.ordered_channels = subtier_channels
+            return subtier_channels
+
+        handler = ranks.RanksHandler()
+        channels = []
+        for tier in handler.ranks:
+            channels += add_subtier_channels(
+                playlist_name=self.name,
+                tier_data=tier
+            )
+
+        return channels
+
+    def run(self):
+        return None
+
+
+class QueuePlaylist(YoutubePlaylist):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tiers = {}
+        self.ranks = ranks.RanksHandler()
+
     def sort(self):
-        ranks_handler = ranks.RanksHandler()
-        ordered_channel_ids = ranks_handler.get_ordered_channels(self.tier)
+        channels = {}
+        for vid_data in self.videos:
+            snippet = vid_data['snippet']
+            if snippet['channelTitle'] not in channels:
+                channels[snippet['channelTitle']] = []
+            channels[snippet['channelTitle']].append(snippet)
+
+        # for tier_data in self.ranks.ranks:
+        #     tier = tier_data['tier']
+        #     if tier not in self.tiers:
+        #         self.tiers[tier] = []
+        #     ordered_channel_ids = self.ranks.get_playlists_and_channels(tier)
+        #     utilities.print_json(ordered_channel_ids)
+        #     for tier in ordered_channel_ids:
+        #         for channel_id in ordered_channel_ids[tier]:
+        #             if channel_id in channels:
+        #                 for snippet in channels[channel_id]:
+        #                     self.tiers[tier].append("%s: %s" % (snippet['channelTitle'], snippet['title']))
 
 
 class SortHandler:
     def __init__(self, **kwargs):
         config = utilities.ConfigHandler()
         self.ranks = ranks.RanksHandler()
+        self.ranks.get_tiers()
+        self.tier_names = kwargs['tiers']
+        self.queue_id = self.ranks.playlists['queue']
+        self.watch_later_id = self.ranks.playlists['watch_later']
+        self.backlog_id = self.ranks.playlists['backlog']
+        self.queue = QueuePlaylist(name='queue', id=self.queue_id)
+        self.tier_playlist_ids = {}
+        self.tier_playlists = {}
+
+    def import_queue(self):
+        self.queue.get_items()
+        self.queue.sort()
+
+    def get_all_tier_playlist_ids(self):
+        counter = 0
+        self.tier_playlist_ids = {}
+        for tier in self.ranks.tiers:
+            if tier.name in self.tier_names:
+                if tier.name == 'primary':
+                    if tier.playlist_id is None:
+                        self.tier_playlist_ids[tier.name] = self.watch_later_id
+                    else:
+                        self.tier_playlist_ids[tier.name] = tier.playlist_id
+                    counter += 1
+                else:
+                    counter = self.get_tier_playlists(tier, counter)
+
+        return self.tier_playlist_ids
+
+    def get_all_playlists(self):
+        if len(self.tier_playlist_ids) == 0:
+            self.get_all_tier_playlist_ids()
+
+        self.tier_playlists = {}
+        for tier_name in self.tier_playlist_ids:
+            playlist_id = self.tier_playlist_ids[tier_name]
+            self.tier_playlists[tier_name] = YoutubePlaylist(id=playlist_id)
+
+        return self.tier_playlists
+
+    def get_tier_playlists(self, tier, counter, parent_playlist_id=None):
+        if tier.playlist_id is None:
+            tier.playlist_id = parent_playlist_id
+        self.tier_playlist_ids[tier.name] = tier.playlist_id
+        counter += 1
+
+        if len(tier.subtiers) > 0:
+            for subtier in tier.subtiers:
+                counter = self.get_tier_playlists(subtier, counter, tier.playlist_id)
+
+        return counter
