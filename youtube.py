5b6d07a8e97e6064cf6f6b679e3d827c4d1e1b80
Michael Jarantilla <michaelj@dogvacay.com>
2020-12-04 23:32:55 -0800

    updated subscriptions.json

--- youtube.py
+++ youtube.py
@@ -4,7 +4,7 @@ from handlers import client
 def main():
     handler = client.YoutubeClientHandler()
 
-    request = handler.client.channels().list(
+    request = handler.client.sorted_channels().list(
         part="snippet,contentDetails,statistics",
         id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
     )
