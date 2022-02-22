dc80828b5c99a118d9dc582682e3a6653b74866e
mjaranti <michael.e.jarantilla@jpl.nasa.gov>
2022-02-22 12:33:24 -0800

    made config into an internal variable for the Cache class

--- handlers/cache.py
+++ handlers/cache.py
@@ -20,8 +20,8 @@ class Cache:
                         provided by the config variables.
         """
         logger.write("Initializing cache")
-        config = ConfigHandler()
-        self.dir = config.variables['CACHE_DIR']
+        self.config = ConfigHandler()
+        self.dir = self.config.variables['CACHE_DIR']
         self.file = os.path.join(self.dir, file)
         self.data = None
 
@@ -131,7 +131,7 @@ class VideoCache(MapCache):
 
     def _check_if_private(self, vid_id, response_obj):
         # Load data about videos marked "private" on YouTube
-        private_videos_file = ConfigHandler().variables['PRIVATE_VIDEOS_FILE']
+        private_videos_file = self.config.variables['PRIVATE_VIDEOS_FILE']
         private_fp = open(private_videos_file, mode='r')
         private_vids_data = json.load(private_fp)
         private_videos = private_vids_data['private_videos']
@@ -169,6 +169,8 @@ class VideoCache(MapCache):
                 self.data[vid_id]['date_cached'] = datetime.datetime.now().timestamp()
 
             return vid_data
+        else:
+            self.data[vid_id] = None
 
         return None
 
@@ -181,7 +183,7 @@ class VideoCache(MapCache):
         @return:        None if add==False and vid_id is not in the cache, OR video metadata if vid_id is in cache
         """
         msg = "Checking cache.... "
-        if vid_id not in self.data:
+        if vid_id not in self.data or (vid_id in self.data and self.data[vid_id] is None):
             msg += "Not found. "
             if update:
                 vid_data = self.add_video(vid_id)
