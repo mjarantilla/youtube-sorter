363e5005b4223f45113a73ab23ff87e2795a3f29
mjaranti <michael.e.jarantilla@jpl.nasa.gov>
2022-04-11 21:19:17 -0700

    hopefuly fixed private video bug

--- handlers/videos.py
+++ handlers/videos.py
@@ -12,17 +12,24 @@ class Video:
 
         @param id:  The YouTube-assigned unique ID for the video
         """
-        self.id = id
-        self.config = ConfigHandler() if 'config' not in kwargs else kwargs['config']
-        self.cache = cache
-        self.private = self._check_if_private()
-        self.client = YoutubeClientHandler() if 'client' not in kwargs else kwargs['client']
-        self.data = self.cache.check_cache(self.id, update=True)
-        self.channel_name = self.data['snippet']['channelTitle']
-        self.channel_id = self.data['snippet']['channelId']
-        self.title = self.data['snippet']['title']
-        self.metadata = None
-        self.duration = self._get_duration()
+        try:
+            self.id = id
+            self.config = ConfigHandler() if 'config' not in kwargs else kwargs['config']
+            self.cache = cache
+            self.private = self._check_if_private()
+            self.client = YoutubeClientHandler() if 'client' not in kwargs else kwargs['client']
+            self.data = self.cache.check_cache(self.id, update=True)
+            self.channel_name = self.data['snippet']['channelTitle']
+            self.channel_id = self.data['snippet']['channelId']
+            self.title = self.data['snippet']['title']
+            self.metadata = None
+            self.duration = self._get_duration()
+        except TypeError as e:
+            logger.write(kwargs)
+            raise
+        except KeyError as e:
+            logger.write(kwargs)
+            raise
 
     def _check_if_private(self):
         # Load data about videos marked "private" on YouTube
