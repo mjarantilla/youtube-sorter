12848c1340d60fa9715a482fd7780869c9f5085d
mjaranti <mjarantilla@gmail.com>
2023-11-02 12:30:41 -0700

    created new script: remove shorts

--- remove_shorts.py
+++ remove_shorts.py
@@ -5,27 +5,13 @@ from handlers.ranks import RanksHandler
 ranks = RanksHandler()
 
 def main(test=False):
-    organizer.import_queue(category="primary", test=test)
-    organizer.import_queue(category="f1", test=test, date_sorting=True)
+    remove_shorts(category="primary", test=test)
+    remove_shorts(category="f1", test=test, date_sorting=True)
 
-def import_queue(category='primary', test=False, max_length: int=None, filler_length: int=None, date_sorting: bool=False):
-    playlists = [
-        {
-            "name": "primary",
-            "id": ranks.data['playlist_ids'][category]
-        },
-        {
-            "name": "queue",
-            "id": ranks.data['queues'][category]
-        },
-        {
-            "name": "backlog",
-            "id": ranks.data['backlogs'][category]
-        }
-    ]
-    playlist_map = {}
-    for playlist in playlists:
-        playlist_map[playlist['name']] = playlist['id']
+def remove_shorts(category='primary', test=False):
+    playlist_map = {
+        "queue": ranks.data['queues'][category]
+    }
 
     fetched_playlists = organizer.fetch_playlists(playlist_map)
 
